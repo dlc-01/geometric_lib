5edd8a6a11a6265d3584d6c7bd5d921a1c0bd2e9
DLC <shabrov23.02@gmail.com>
2023-12-11 23:36:06 +0300

    refactor:: test

--- test_rectangle.py
+++ test_rectangle.py
@@ -4,24 +4,51 @@ from rectangle import area
 
 
 class TestRectangleFunctions(unittest.TestCase):
-    def test_area(self):
+    """
+    Test rectangle function.
+    """
+    def test_rectangle_area_integer(self):
         """
-        Test the area function.
+        Test the area function by integer.
         """
         # Test for sides a = 10, b = 5
-        self.assertEqual(area(10, 5), 50) 
-        # Test for sides a = 15.2, b = 3.3
-        self.assertAlmostEqual(area(15.2, 3.3), 15.2*3.3)
-        # Test for sides a = 20, b = 50.2
-        self.assertAlmostEqual(area(20, 50.2), 20*50.2)
+        self.assertEqual(area(10, 5), 50)
 
-    def test_perimeter(self):
+
+    def test_rectangle_area_float(self):
         """
-        Test the perimeter function.
+        Test the area function by float
+        """
+        # Test for sides a = 15.2, b = 3.3
+        self.assertEqual(area(15.2, 3.3), 15.2 * 3.3)
+    
+    def test_rectangle_area_float_int(self):
+        """
+        Test the area function by float and int
+        """
+        # Test for sides a = 15.2, b = 2
+        self.assertEqual(area(15.2, 2), 15.2 * 2)
+        
+    
+    def test_rectangle_perimeter_integer(self):
+        """
+        Test the perimeter function by integer.
         """
         # Test for sides a = 10, b = 5
         self.assertAlmostEqual(perimeter(10, 5), 30)
+
+
+    def test_rectangle_perimeter_float(self):
+        """
+        Test the perimeter function by float
+        """
         # Test for sides a = 15.2, b = 3.3
         self.assertAlmostEqual(perimeter(15.2, 3.3), 2*(15.2+3.3))
+    
+    def test_rectangle_perimeter_float_int(self):
+        """
+        Test the perimeter function by float and int
+        """
         # Test for sides a = 20, b = 50.2
         self.assertAlmostEqual(perimeter(20, 50.2), 2*(50.2+20))
+        
--- test_square.py
+++ test_square.py
@@ -3,29 +3,52 @@ from square import area, perimeter
 
 
 class TestSquareFunctions(unittest.TestCase):
-    def test_area(self):
+    """
+    Test square function.
+    """
+    def test_square_area_integer(self):
         """
-        Tests the area function.
+        Test the area function by integer.
         """
         # Test for side a = 4
         self.assertAlmostEqual(area(4), 16)
 
+
+    def test_square_area_float(self):
+        """
+        Test the area function by float
+        """
         # Test for side a = 6.9
         self.assertAlmostEqual(area(6.9), 6.9*6.9)
-
-        # Test for side a = 52
-        self.assertAlmostEqual(area(52), 52*52)
-
-    def test_perimeter(self):
+    
+    def test_square_area_big(self):
         """
-        Test the perimeter function with different input values.
+        Test the area function by float and int
+        """
+        # Test for side a = 52
+        self.assertAlmostEqual(area(520), 520*520)
+        
+    
+    def test_square_perimeter_integer(self):
+        """
+        Test the perimeter function by integer.
         """
         # Test for side a = 5
         self.assertAlmostEqual(perimeter(5), 20)
 
+
+    def test_square_perimeter_float(self):
+        """
+        Test the area function by float
+        """
         # Test for side a = 6.9
         self.assertAlmostEqual(perimeter(6.9), 4*6.9)
-
-        # Test for side a = 52
-        self.assertAlmostEqual(perimeter(52), 4*52)
+    
+    def test_square_perimeter_big(self):
+        """
+        Test the area function by float and int
+        """
+        # Test for side a = 520
+        self.assertAlmostEqual(perimeter(520), 4*520)
+        
 
